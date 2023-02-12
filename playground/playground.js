064ab5e25930b6ff3872471af6474640ee2e06e7
vrunoa <vruno.alassia@gmail.com>
2023-02-11 16:50:42 -0800

    fix lint

--- playground/playground.js
+++ playground/playground.js
@@ -1,9 +1,12 @@
 (async () => {
-  const AppiumBoostPlugin = require('../dist/index').default;
+  const { AppiumBoostPlugin } = require('../dist/index');
   const { sleep } = require('asyncbox');
   const plugin = new AppiumBoostPlugin();
   await plugin.boostConnect();
   await plugin.boostDrive();
+  await sleep(3500);
+  await plugin.boostBrake();
+  await plugin.boostReverse();
   await sleep(3500);
   await plugin.boostBrake();
   await plugin.boostDisconnect();
